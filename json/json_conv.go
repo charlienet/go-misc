5dae078b200e18c33816b8ff53fd700d3dbbe483
charlie <3140647@qq.com>
2024-09-11 12:55:31 +0800

    json

---
+++ json/json_conv.go
@@ -0,0 +1,102 @@
+package json
+
+import (
+	"regexp"
+
+	"github.com/charlienet/misc/bytesconv"
+	"github.com/charlienet/misc/stringx"
+)
+
+var (
+	keyMatchRegex = regexp.MustCompile(`\"(\w+)\":`)
+)
+
+type Snake2Camel struct{ Value any }
+
+func (c Snake2Camel) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Snake2Camel(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
+
+type Snake2Pascal struct{ Value any }
+
+func (c Snake2Pascal) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Snake2Pascal(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
+
+type Pascal2Camel struct{ Value any }
+
+func (c Pascal2Camel) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Pascal2Camel(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
+
+type Camel2Pascal struct{ Value any }
+
+func (c Camel2Pascal) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Camel2Pascal(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
+
+type Pascal2Snake struct{ Value any }
+
+func (c Pascal2Snake) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Pascal2Snake(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
+
+type Pascal2UpperSnake struct{ Value any }
+
+func (c Pascal2UpperSnake) MarshalJSON() ([]byte, error) {
+	marshalled, err := Marshal(c.Value)
+	converted := keyMatchRegex.ReplaceAllFunc(
+		marshalled,
+		func(match []byte) []byte {
+			matchStr := bytesconv.BytesToString(match[1 : len(match)-2])
+			name := stringx.Pascal2UpperSnake(matchStr)
+			return []byte(`"` + name + `":`)
+		})
+
+	return converted, err
+}
