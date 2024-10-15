824b1cc1e7b50a7942026a823fcbab20bc81308b
charlie <3140647@qq.com>
2024-10-15 15:24:08 +0800

    type

--- random/random.go
+++ random/random.go
@@ -6,7 +6,8 @@ import (
 )
 
 type scopeConstraint interface {
-	~int | ~int32 | ~int64 | ~uint32
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 func Int[T scopeConstraint]() T {
