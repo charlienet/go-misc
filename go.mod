2ddb0991171b2e57a553613c9d99f2ddd11756dc
charlie <3140647@qq.com>
2025-08-26 14:48:13 +0800

    nacos

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25
 require (
 	github.com/cespare/xxhash/v2 v2.3.0
 	github.com/json-iterator/go v1.1.12
+	github.com/nacos-group/nacos-sdk-go/v2 v2.3.3
 	github.com/shopspring/decimal v1.4.0
 	github.com/spaolacci/murmur3 v1.1.0
 	github.com/stretchr/testify v1.11.0
@@ -12,11 +13,60 @@ require (
 )
 
 require (
+	github.com/alibabacloud-go/alibabacloud-gateway-pop v0.1.0 // indirect
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.5 // indirect
+	github.com/alibabacloud-go/darabonba-array v0.1.0 // indirect
+	github.com/alibabacloud-go/darabonba-encode-util v0.0.2 // indirect
+	github.com/alibabacloud-go/darabonba-map v0.0.2 // indirect
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.1.11 // indirect
+	github.com/alibabacloud-go/darabonba-signature-util v0.0.7 // indirect
+	github.com/alibabacloud-go/darabonba-string v1.0.2 // indirect
+	github.com/alibabacloud-go/debug v1.0.1 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.1 // indirect
+	github.com/alibabacloud-go/kms-20160120/v3 v3.3.1 // indirect
+	github.com/alibabacloud-go/openapi-util v0.1.1 // indirect
+	github.com/alibabacloud-go/tea v1.3.11 // indirect
+	github.com/alibabacloud-go/tea-utils v1.4.5 // indirect
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.7 // indirect
+	github.com/alibabacloud-go/tea-xml v1.1.3 // indirect
+	github.com/aliyun/alibaba-cloud-sdk-go v1.63.107 // indirect
+	github.com/aliyun/alibabacloud-dkms-gcs-go-sdk v0.5.1 // indirect
+	github.com/aliyun/alibabacloud-dkms-transfer-go-sdk v0.1.9 // indirect
+	github.com/aliyun/aliyun-secretsmanager-client-go v1.1.5 // indirect
+	github.com/aliyun/credentials-go v1.4.7 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/clbanning/mxj/v2 v2.7.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
+	github.com/orcaman/concurrent-map v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.23.0 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.65.0 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250825161204-c5933d9347a5 // indirect
+	google.golang.org/grpc v1.75.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ nacos/nacos.go
@@ -0,0 +1,254 @@
+package nacos
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/model"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+)
+
+var (
+	ErrConfigNotFound = errors.New("config not found")
+	ErrPublishFailed  = errors.New("publish failed")
+)
+
+type NacosConfig struct {
+	Host      string // nacos 地址
+	Port      int    // nacos 端口
+	GrpcPort  int    // nacos grpc 端口
+	Namespace string // nacos 命名空间
+	Group     string // nacos 配置分组
+	CacheDir  string
+	LogDir    string
+}
+
+type NacosClient struct {
+	cfg          NacosConfig
+	nameClient   naming_client.INamingClient
+	configClient config_client.IConfigClient
+}
+
+type options struct {
+	Group string
+	Type  string
+}
+
+type option func(*options)
+
+func NewNacosClient(cfg NacosConfig) (*NacosClient, error) {
+	clientCofnig := constant.ClientConfig{
+		NamespaceId:         cfg.Namespace,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              cfg.LogDir,
+		CacheDir:            cfg.CacheDir,
+	}
+
+	serverConfigs := []constant.ServerConfig{
+		{
+			IpAddr:   cfg.Host,
+			Port:     uint64(cfg.Port),
+			GrpcPort: uint64(cfg.GrpcPort),
+		},
+	}
+
+	nameClient, err := clients.CreateNamingClient(map[string]any{
+		"serverConfigs": serverConfigs,
+		"clientConfig":  clientCofnig,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	configClient, err := clients.CreateConfigClient(map[string]any{
+		"serverConfigs": serverConfigs,
+		"clientConfig":  clientCofnig,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &NacosClient{
+		cfg:          cfg,
+		nameClient:   nameClient,
+		configClient: configClient,
+	}, nil
+}
+
+func WithType(typ string) option {
+	return func(o *options) {
+		o.Type = typ
+	}
+}
+
+func WithGroup(group string) option {
+	return func(o *options) {
+		o.Group = group
+	}
+}
+
+type Instance struct {
+	ServiceName string
+	IP          string
+	Port        uint64
+	Weight      float64
+}
+
+func (n *NacosClient) RegisterInstance(i Instance, opts ...option) (bool, error) {
+	opt := n.getOpt(opts...)
+	return n.nameClient.RegisterInstance(vo.RegisterInstanceParam{
+		Ip:          i.IP,
+		Port:        i.Port,
+		ServiceName: i.ServiceName,
+		Weight:      i.Weight,
+		GroupName:   opt.Group,
+		Enable:      true,
+		Healthy:     true,
+	})
+}
+
+func (n *NacosClient) BatchRegisterInstance(instances []Instance, opts ...option) (bool, error) {
+	opt := n.getOpt(opts...)
+
+	if len(instances) == 0 {
+		return false, errors.New("instances is empty")
+	}
+
+	param := vo.BatchRegisterInstanceParam{
+		ServiceName: instances[0].ServiceName,
+		GroupName:   opt.Group,
+	}
+	param.Instances = make([]vo.RegisterInstanceParam, 0, len(instances))
+	for _, i := range instances {
+		param.Instances = append(param.Instances, vo.RegisterInstanceParam{
+			Ip:          i.IP,
+			Port:        i.Port,
+			ServiceName: i.ServiceName,
+			Weight:      i.Weight,
+			GroupName:   opt.Group,
+			Enable:      true,
+		})
+	}
+
+	return n.nameClient.BatchRegisterInstance(param)
+}
+
+func (n *NacosClient) DeregisterInstance(i Instance, opts ...option) (bool, error) {
+	opt := n.getOpt(opts...)
+	return n.nameClient.DeregisterInstance(vo.DeregisterInstanceParam{
+		Ip:          i.IP,
+		Port:        i.Port,
+		ServiceName: i.ServiceName,
+		GroupName:   opt.Group,
+	})
+}
+
+func (n *NacosClient) GetService(serviceName string, opts ...option) (model.Service, error) {
+	opt := n.getOpt(opts...)
+	return n.nameClient.GetService(vo.GetServiceParam{
+		ServiceName: serviceName,
+		GroupName:   opt.Group,
+	})
+}
+
+func (n *NacosClient) SelectAllInstances(serviceName string, opts ...option) ([]model.Instance, error) {
+	opt := n.getOpt(opts...)
+
+	return n.nameClient.SelectAllInstances(vo.SelectAllInstancesParam{
+		ServiceName: serviceName,
+		GroupName:   opt.Group,
+	})
+}
+
+func (n *NacosClient) SelectInstances(serviceName string, opts ...option) ([]model.Instance, error) {
+	opt := n.getOpt(opts...)
+	instances, err := n.nameClient.SelectInstances(vo.SelectInstancesParam{
+		ServiceName: serviceName,
+		GroupName:   opt.Group,
+		HealthyOnly: true,
+	})
+
+	return instances, err
+}
+
+func (n *NacosClient) SelectOneHealthyInstance(serviceName string, opts ...option) (*model.Instance, error) {
+	opt := n.getOpt(opts...)
+	return n.nameClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
+		ServiceName: serviceName,
+		GroupName:   opt.Group,
+	})
+}
+
+const NumberRetries = 3
+
+func (n *NacosClient) SetConfig(dataId string, content string, opts ...option) error {
+	o := n.getOpt(opts...)
+
+	var err error
+
+	retries := NumberRetries
+	for published := false; !published && retries > 0; retries-- {
+		published, err = n.configClient.PublishConfig(vo.ConfigParam{
+			DataId:  dataId,
+			Group:   o.Group,
+			Type:    o.Type,
+			Content: content,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if published {
+			return nil
+		}
+	}
+
+	return ErrPublishFailed
+}
+
+// 获取配置文件
+func (n *NacosClient) GetConfig(cfg any, dataId string, opts ...option) error {
+	c, err := n.GetConfigString(dataId, opts...)
+	if err != nil {
+		return err
+	}
+
+	if c == "" {
+		return ErrConfigNotFound
+	}
+
+	if err := json.Unmarshal([]byte(c), cfg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *NacosClient) GetConfigString(dataId string, opts ...option) (string, error) {
+	o := n.getOpt(opts...)
+
+	c, err := n.configClient.GetConfig(vo.ConfigParam{
+		DataId: dataId,
+		Group:  o.Group,
+	})
+
+	return c, err
+}
+
+func (n *NacosClient) getOpt(opts ...option) *options {
+	o := &options{
+		Group: n.cfg.Group,
+	}
+
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	return o
+}
